0735ab22e9c392f50d17d924214c419e79622183
M-Usamah <mohammad.usamah127@hotmail.com>
2023-07-16 22:26:09 +0500

    LSTM model has created

---
+++ LSTM_model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        _, (h_n, _) = self.lstm(x)
+        output = self.fc(h_n[-1])
+        return output
